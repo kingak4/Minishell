ab45f8b6aaad701375e265711785fb4310546d5f
kinga <kinga.kwasniak5@gmail.com>
2025-07-07 16:35:51 +0200

    read line supressed and new linke list added

--- Kinga/read_line.c
+++ Kinga/read_line.c
@@ -6,91 +6,75 @@
 /*   By: kikwasni <kikwasni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 11:16:31 by root              #+#    #+#             */
-/*   Updated: 2025/07/03 10:47:33 by kikwasni         ###   ########.fr       */
+/*   Updated: 2025/07/07 16:22:07 by kikwasni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "minishell.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
-typedef struct s_cmd
-{
-	char	*line;
-	char	**redi_file;
-	int		*redi_signal;
-	int		count_redi;
-}	t_cmd;
-
-t_cmd *read_cmd(t_cmd *read)
+// uzwaj make valgrind 
+t_pars *read_cmd(t_pars *read)
 {
 	if (!read)
 		return (NULL);
-	read->line = readline("minishell");
+	read->line = readline("minishell>");
 	if (!read->line)
 		return (NULL);
 	return (read);
 }
-int check_redirect_error(t_cmd *read, int pos)
-{
-	int count;
-	int i;
-	char c;
+//int check_redirect_error(t_cmd *read, int pos)
+//{
+//	int count;
+//	int i;
+//	char c;
 
-	if (!read || !read->line)
-		return (0);
-	count = 0;
-	i = pos;
-	c = read->line[pos];
-	while (read->line[i] && read->line[i] == c)
-	{
-		count++;
-		i++;
-	}
-	if (count >= 3)
-	{
-		write(2, "minishell: syntax error near unexpected token '", 46);
-		write(2, &read->line[pos], count);
-		write(2, "'\n", 2);
-		return (0);
-	}
-	return (1);
-}
-t_cmd	*count_redirections(t_cmd *read)
-{
-	int	i;
+//	if (!read || !read->line)
+//		return (0);
+//	count = 0;
+//	i = pos;
+//	c = read->line[pos];
+//	while (read->line[i] && read->line[i] == c)
+//	{
+//		count++;
+//		i++;
+//	}
+//	if (count >= 3)
+//	{
+//		write(2, "minishell: syntax error near unexpected token '", 46);
+//		write(2, &read->line[pos], count);
+//		write(2, "'\n", 2);
+//		return (0);
+//	}
+//	return (1);
+//}
+//t_cmd	*count_redirections(t_cmd *read)
+//{
+//	int	i;
 
-	i = 0;
-	read->count_redi = 0;
-	while (read->line[i] != '\0')
-	{
-		if (read->line[i] == '>' && read->line[i + 1] != '\0'
-			&& read->line[i + 1] == '>')
-			{
-				read->count_redi++;
-				i++;
-			}
-		else if (read->line[i] == '>')
-			read->count_redi++;
-		else if (read->line[i] == '<' && read->line[i + 1] != '\0'
-			&& read->line[i + 1] == '<')
-			{
-				read->count_redi++;
-				i++;
-			}
-		else if (read->line[i] == '<')
-			read->count_redi++;
-		i++;
-	}
-	return (read);
-}
-// do naprawy szkieliet to jest
+//	i = 0;
+//	read->count_redi = 0;
+//	while (read->line[i] != '\0')
+//	{
+//		if (read->line[i] == '>' && read->line[i + 1] != '\0'
+//			&& read->line[i + 1] == '>')
+//			{
+//				read->count_redi++;
+//				i++;
+//			}
+//		else if (read->line[i] == '>')
+//			read->count_redi++;
+//		else if (read->line[i] == '<' && read->line[i + 1] != '\0'
+//			&& read->line[i + 1] == '<')
+//			{
+//				read->count_redi++;
+//				i++;
+//			}
+//		else if (read->line[i] == '<')
+//			read->count_redi++;
+//		i++;
+//	}
+//	return (read);
+//}
 //t_cmd *parse_redirect(t_cmd *read, int pos)
 //{
 //	int size;
@@ -132,7 +116,7 @@ t_cmd	*count_redirections(t_cmd *read)
 // }
 //int main(void)
 //{
-//	t_cmd cmd;
+//	t_pars cmd;
 
 //	while (1)
 //	{
@@ -149,30 +133,4 @@ t_cmd	*count_redirections(t_cmd *read)
 //	rl_clear_history();
 //	return 0;
 //}
-int main(void)
-{
-    t_cmd cmd;
-    char *input;
 
-    while (1)
-    {
-        input = readline("minishell> ");
-        if (!input)
-        {
-            printf("\nKoniec wejścia. Kończę program.\n");
-            break;
-        }
-
-        cmd.line = input;
-        cmd.count_redi = 0;
-
-        count_redirections(&cmd);
-
-        printf("Linia: \"%s\"\n", cmd.line);
-        printf("Znaleziono redirekcji: %d\n\n", cmd.count_redi);
-
-        free(input);
-    }
-
-    return 0;
-}
--- Makefile
+++ Makefile
@@ -3,26 +3,27 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: kdyga <kdyga@student.42.fr>                +#+  +:+       +#+         #
+#    By: kikwasni <kikwasni@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/01 19:22:14 by kdyga             #+#    #+#              #
-#    Updated: 2025/07/01 19:24:10 by kdyga            ###   ########.fr        #
+#    Updated: 2025/07/07 16:00:28 by kikwasni         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = cc
 CFLAGS = -Wall -Wextra -Werror -g -I minishell/
+LDFLAGS = -lreadline
 NAME = mini
 
-SRCS = main/main.c command/cd.c
+SRCS = main/main.c Kamil/command/cd.c Kinga/read_line.c
 OBJS = $(SRCS:.c=.o)
 
-.PHONY: all clean fclean re
+.PHONY: all clean fclean re valgrind
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME) $(LDFLAGS)
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
@@ -35,3 +36,5 @@ fclean: clean
 
 re: fclean all
 
+valgrind: $(NAME)
+	valgrind --leak-check=full --suppressions=readline.supp ./$(NAME)
